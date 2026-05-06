a3b9ad27acf847e35530fc5ef773ef452a8add23
salmanida <salmanidauljannah@gmail.com>
2026-05-06 15:03:11 +0700

    feat: add WhatsApp direct message link

--- app/page.tsx
+++ app/page.tsx
@@ -523,7 +523,7 @@ export default function Home() {
               </div>
               <div>
                 <p className="text-sm font-semibold text-slate-900">Phone</p>
-                <p className="mt-2 text-slate-600">+62 851-4844-3939</p>
+                <a href="https://wa.me/+6285148443939" className="hover:underline"><p className="mt-2 text-slate-600">+62 851-4844-3939</p></a>
               </div>
               <div>
                 <p className="text-sm font-semibold text-slate-900">Location</p>
